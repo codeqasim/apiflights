0aaab87d20fd9fd51c862fb50b9538c43b1abbdf
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2022-01-13 03:21:08 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,185 @@
-contact
+<?php 
+$title = "Contact Us";
+include "header.php";
+?>
+
+<div class="jsx-3626727052 page page--purple">
+<main class="jsx-3626727052">
+<div class="LayoutGrid_layout-grid__40eRT ContactUsPageView_contact-us-page-view-container__1lrni">
+    <section class="VSpace_v-space--lg__2NUNZ Copy_copy-section__2z4Xk">
+        <div class="VSpace_v-space--xs__20V_H">
+        <div class="Text_text__MZ693 Text_text--grey-200__3qvOE Text_text--caption__3_prN Text_text--left__BBdcz">contact sales</div>
+        <h1 class="Text_text__MZ693 Text_text--purple-200__Tu0tA Text_text--d1__3ZGLu Text_text--left__BBdcz">Get in touch</h1>
+        </div>
+        <div class="VSpace_v-space--xl__1tMOz">
+        <p class="Text_text__MZ693 Text_text--white__3wIlg Text_text--body-large__2BeyV Text_text--left__BBdcz">Tell us a little about you and your company. Our team will get back to you as soon as possible to answer your questions or set up a call.</p>
+        </div>
+    </section>
+    <form class="ContactForm_contact-form-container__3Tm_b">
+        <div class="VSpace_v-space--md__kcJZY">
+        <div id="firstName-form-field" class="FormField_form-field__15jSm">
+            <label for="firstName" class="FormField_form-field__label__3BMQP">First name<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="firstName-input-container" class="Input_input__container__3sEKm">
+                <input placeholder="Mae" class="Input_input__r5ePL" id="firstName" name="firstName" autoComplete="given-name" value=""/>
+                <span role="img" aria-label="Error icon displayed for: &quot;undefined&quot;" class="Input_error-icon__j6CYS">
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+        <div id="lastName-form-field" class="FormField_form-field__15jSm">
+            <label for="lastName" class="FormField_form-field__label__3BMQP">Last name<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="lastName-input-container" class="Input_input__container__3sEKm">
+                <input placeholder="Jemisen" class="Input_input__r5ePL" id="lastName" name="lastName" autoComplete="family-name" value=""/>
+                <span role="img" aria-label="Error icon displayed for: &quot;undefined&quot;" class="Input_error-icon__j6CYS">
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+        <div id="email-form-field" class="FormField_form-field__15jSm">
+            <label for="email" class="FormField_form-field__label__3BMQP">Email address<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="email-input-container" class="Input_input__container__3sEKm">
+                <input type="email" placeholder="mae@jemisen.com" class="Input_input__r5ePL" id="email" name="email" autoComplete="email" value=""/>
+                <span role="img" aria-label="Error icon displayed for: &quot;undefined&quot;" class="Input_error-icon__j6CYS">
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+        <div id="function-form-field" class="FormField_form-field__15jSm">
+            <label for="function" class="FormField_form-field__label__3BMQP">Function<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="function-select-container" class="Select_select__container__RCZin">
+                <select required="" id="function" name="function" placeholder="Select a company function" class="Select_select__3GV-x">
+                    <option selected="" value="" disabled="" class="Select_select__placeholder__2e8O4">Select a company function</option>
+                    <option value="Developer">Developer</option>
+                    <option value="Management">Management</option>
+                    <option value="Product">Product</option>
+                    <option value="Sales &amp; Marketing">Sales &amp; Marketing</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+        </div>
+        <div id="companyName-form-field" class="FormField_form-field__15jSm">
+            <label for="companyName" class="FormField_form-field__label__3BMQP">Company name<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="companyName-input-container" class="Input_input__container__3sEKm">
+                <input type="companyName" placeholder="Jemisen" class="Input_input__r5ePL" id="companyName" name="companyName" autoComplete="organization" value=""/>
+                <span role="img" aria-label="Error icon displayed for: &quot;undefined&quot;" class="Input_error-icon__j6CYS">
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+        <div id="companySize-form-field" class="FormField_form-field__15jSm">
+            <label for="companySize" class="FormField_form-field__label__3BMQP">Company size<span class="FormField_required-asterisk__3PS0Q"> *</span></label>
+            <div id="companySize-select-container" class="Select_select__container__RCZin">
+                <select required="" id="companySize" name="companySize" placeholder="Company size" class="Select_select__3GV-x">
+                    <option selected="" value="" disabled="" class="Select_select__placeholder__2e8O4">Company size</option>
+                    <option value="Just me">Just me</option>
+                    <option value="2-49">2-49</option>
+                    <option value="50-149">50-149</option>
+                    <option value="150-299">150-299</option>
+                    <option value="300-2999">300-2999</option>
+                    <option value="3000+">3000+</option>
+                </select>
+            </div>
+        </div>
+        <div id="bookingVolume-form-field" class="FormField_form-field__15jSm">
+            <label for="bookingVolume" class="FormField_form-field__label__3BMQP">
+                Total volume<span class="FormField_required-asterisk__3PS0Q"> *</span>
+                <span>
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon FormField_info-tooltip-icon__3vJGd">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 15c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1-8h-2V7h2v2z"></path>
+                    </svg>
+                </span>
+            </label>
+            <div id="bookingVolume-select-container" class="Select_select__container__RCZin">
+                <select required="" id="bookingVolume" name="bookingVolume" placeholder="Select a monthly amount" class="Select_select__3GV-x">
+                    <option selected="" value="" disabled="" class="Select_select__placeholder__2e8O4">Select a monthly amount</option>
+                    <option value="None, just starting out">None, just starting out</option>
+                    <option value="Less than £24,999">Less than £24,999</option>
+                    <option value="£25,000-£99,999">£25,000-£99,999</option>
+                    <option value="£100,000-£499,999">£100,000-£499,999</option>
+                    <option value="£500,000-£999,999">£500,000-£999,999</option>
+                    <option value="£1,000,000+">£1,000,000+</option>
+                </select>
+            </div>
+        </div>
+        <div id="message-form-field" class="FormField_form-field__15jSm">
+            <label for="message" class="FormField_form-field__label__3BMQP">Anything else?</label>
+            <div id="message-textarea-container" class="TextArea_textarea__container__25jWC">
+                <textarea rows="5" id="message" name="message" placeholder="Tell us more about your timeline, airline requirements,  and what you&#x27;re looking for." class="TextArea_textarea__1t4ad"></textarea>
+                <span role="img" aria-label="Error icon displayed for: &quot;undefined&quot;" class="TextArea_error-icon__21KbF">
+                    <svg width="15" height="15" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+        <button class="Button_button__20bBc Button_button--dark__3Sla1 Button_button--fill__2ijRd" type="submit" disabled="">Submit</button>
+        </div>
+        <div class="ContactForm_calendly-card-backdrop__3neqR">
+        <div class="VSpace_v-space--sm__ESC07 CalendlyCard_calendly-card__3ixS4">
+            <div class="CalendlyCard_calendly-card__close-button-container__cSVYj">
+                <button type="button" class="CalendlyCard_calendly-card__close-button__2h0hv">
+                    <svg width="24" height="24" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"></path>
+                    </svg>
+                </button>
+            </div>
+            <div class="VSpace_v-space--lg__2NUNZ">
+                <svg width="36" height="36" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon CalendlyCard_calendly-card__phone-icon__1QH7g">
+                    <path d="M20.51 15.38C19.28 15.38 18.09 15.18 16.98 14.82C16.63 14.7 16.24 14.79 15.97 15.06L14.4 17.03C11.57 15.68 8.92 13.13 7.51 10.2L9.46 8.54C9.73 8.26 9.81 7.87 9.7 7.52C9.33 6.41 9.14 5.22 9.14 3.99C9.14 3.45 8.69 3 8.15 3H4.69C4.15 3 3.5 3.24 3.5 3.99C3.5 13.28 11.23 21 20.51 21C21.22 21 21.5 20.37 21.5 19.82V16.37C21.5 15.83 21.05 15.38 20.51 15.38Z"></path>
+                </svg>
+                <div class="VSpace_v-space--xl__1tMOz">
+                    <p class="Text_text__MZ693 Text_text--h4__1M48f Text_text--center__2g-e9">
+                    Interested in <b class="CalendlyCard_semi-bold__125d1">booking a call</b> <!-- -->with our sales team today? Select a date and time below.
+                    </p>
+                    <div><a class="Button_button__20bBc Button_button--dark__3Sla1 Button_button--fill__2ijRd Button_button--medium__3XYQZ" href="https://calendly.com/mattatduffel/20-mins-with-matt" rel="noopener noreferrer" target="_blank">Book now</a></div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </form>
+    <section class="VSpace_v-space--2xl__2-8xE HelpCards_help-cards-container__1Vllc">
+        <h4 class="Text_text__MZ693 Text_text--white__3wIlg Text_text--h4__1M48f Text_text--left__BBdcz">Looking for resources or support?</h4>
+        <div class="HSpace_h-space__1C_k1 HSpace_h-space--md__1c4B3">
+        <div class="VSpace_v-space--md__kcJZY HelpCards_help-card__39ZaG">
+            <div class="HSpace_h-space__1C_k1 HSpace_h-space--sm__24TZe HSpace_h-space--align-center__3ayGS">
+                <span role="img" aria-label="help_center">
+                    <svg width="24" height="24" viewBox="0 0 24 24" class="jsx-4213353732 ff-icon">
+                    <rect fill="none" height="24" width="24"></rect>
+                    <path d="M19,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V5C21,3.9,20.1,3,19,3z M12.01,18 c-0.7,0-1.26-0.56-1.26-1.26c0-0.71,0.56-1.25,1.26-1.25c0.71,0,1.25,0.54,1.25,1.25C13.25,17.43,12.72,18,12.01,18z M15.02,10.6 c-0.76,1.11-1.48,1.46-1.87,2.17c-0.1,0.18-0.16,0.32-0.19,0.63c-0.05,0.45-0.45,0.78-0.9,0.78H12c-0.52,0-0.93-0.44-0.88-0.96 c0.03-0.34,0.11-0.69,0.3-1.03c0.49-0.87,1.42-1.39,1.96-2.16c0.57-0.81,0.25-2.33-1.37-2.33c-0.71,0-1.18,0.36-1.47,0.79 c-0.25,0.36-0.69,0.53-1.1,0.36l0,0C8.91,8.64,8.72,8,9.04,7.54C9.65,6.65,10.67,6,11.99,6c1.48,0,2.49,0.67,3.01,1.52 C15.44,8.24,15.7,9.59,15.02,10.6z"></path>
+                    </svg>
+                </span>
+                <p class="Text_text__MZ693 Text_text--white__3wIlg Text_text--body__3bgPZ Text_text--left__BBdcz">General Enquiries</p>
+            </div>
+            <p class="Text_text__MZ693 Text_text--grey-300__3X70k Text_text--label__13ggu Text_text--left__BBdcz">
+                Check out our<!-- --> <a class="Anchor_anchor__u3Dra" href="https://help.duffel.com/" target="_blank" rel="noopener noreferrer">Help Centre</a> <!-- -->to get answers to common questions. Topics include: getting started with Duffel, our API, content and features, airline partnerships, and more. Need more support? Reach out at <a class="Anchor_anchor__u3Dra">help@duffel.com</a>.
+            </p>
+        </div>
+        <div class="VSpace_v-space--md__kcJZY HelpCards_help-card__39ZaG">
+            <div class="HSpace_h-space__1C_k1 HSpace_h-space--sm__24TZe HSpace_h-space--align-center__3ayGS">
+                <span role="img" aria-label="api-reference">
+                    <svg class="BrandingIcon_ff-branding-icon__V-I1Z" width="24" height="24" viewBox="0 0 24 24">
+                    <title>api-reference</title>
+                    <path d="M12.9838 12.9838C12.4329 13.5347 11.5573 13.5347 11.0162 12.9936V12.9838C10.4752 12.4427 10.4752 11.5671 11.0162 11.0261V11.0162C11.5573 10.4752 12.4329 10.4752 12.9739 11.0162H12.9838C13.5248 11.5573 13.5248 12.4427 12.9838 12.9838ZM12 6.09739L14.0856 8.18298L16.545 5.72356L13.397 2.5755C12.6296 1.80817 11.3802 1.80817 10.6129 2.5755L7.46483 5.72356L9.92425 8.18298L12 6.09739ZM6.09739 12L8.18298 9.91441L5.72356 7.45499L2.5755 10.6031C1.80817 11.3704 1.80817 12.6198 2.5755 13.3871L5.72356 16.5352L8.18298 14.0758L6.09739 12ZM17.9026 12L15.817 14.0856L18.2764 16.545L21.4245 13.397C22.1918 12.6296 22.1918 11.3802 21.4245 10.6129L18.2764 7.46483L15.817 9.92425L17.9026 12ZM12 17.9026L9.91441 15.817L7.45499 18.2764L10.6031 21.4245C11.3704 22.1918 12.6198 22.1918 13.3871 21.4245L16.5352 18.2764L14.0758 15.817L12 17.9026Z"></path>
+                    </svg>
+                </span>
+                <p class="Text_text__MZ693 Text_text--white__3wIlg Text_text--body__3bgPZ Text_text--left__BBdcz">For developers</p>
+            </div>
+            <p class="Text_text__MZ693 Text_text--grey-300__3X70k Text_text--label__13ggu Text_text--left__BBdcz">
+                Access our<!-- --> <a class="Anchor_anchor__u3Dra" href="index">Developer Docs</a> <!-- -->to learn how to use Duffel. Explore our in-depth<!-- --> <a class="Anchor_anchor__u3Dra" href="docs/guides/quick-start">Guides</a>, view our up-to-date<!-- --> <a class="Anchor_anchor__u3Dra" href="docs/api/overview/welcome.html">API Reference</a>, check out our<!-- --> <a class="Anchor_anchor__u3Dra" href="https://changelog.duffel.com/" target="_blank" rel="noopener noreferrer">Changelog</a>, and more.
+            </p>
+        </div>
+        </div>
+    </section>
+</div>
+</main>
+
+
+<?php include "footer.php"; ?>
