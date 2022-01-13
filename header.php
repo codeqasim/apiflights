4323346b9c4cb964a9cac6ee4484f7d8437d09a4
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2022-01-13 05:29:26 +0500

    i

--- header.php
+++ header.php
@@ -1,3 +1,19 @@
+<?php 
+$whitelist = array(
+    '127.0.0.1',
+    '::1'
+);
+
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+    define('root','http://localhost/apiflights/');
+    define('assets','http://localhost/apiflights/public/');
+} else {
+    define('root','https://apiflights.com/');
+    define('assets','https://apiflights.com/');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -20,7 +36,7 @@
       <meta name="twitter:title" content="Duffel - The best API to sell flights online"/>
       <meta name="twitter:description" content="We offer a powerful API that enables any travel business to search, book, and sell flights from a network of the world&#x27;s leading airlines in minutes, not months."/>
       <meta name="next-head-count" content="13"/>
-      <link rel="stylesheet" href="./css/app.css" />
+      <link rel="stylesheet" href="<?=assets?>css/app.css" />
     
       <style id="__jsx-4213353732">.ff-icon.jsx-4213353732{display:block;fill:currentColor;-webkit-transition:all 0.3s cubic-bezier(0.25,0.8,0.25,1);transition:all 0.3s cubic-bezier(0.25,0.8,0.25,1);}</style>
       <style id="__jsx-3626727052">.page.jsx-3626727052{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between;min-height:100vh;}.page--purple.jsx-3626727052{background-color:#100024;}.page--top-padded.jsx-3626727052{padding-top:2rem;}body{background-color:#FFFFFF;color:#231323;undefined;}</style>
@@ -30,30 +46,27 @@
         <header data-selector="fs-show" class="Header_header__3-Q1o" role="banner">
         <nav class="LayoutGrid_layout-grid__40eRT" role="navigation">
         <div class="Header_header__lockup__2IzEg">
-            <a href="./">
-            <svg class="LockUp_lockup__y2HQS" viewBox="0 0 127 30">
-                <path d="M112.843015,12.0070588 C115.487426,12.0070588 116.391176,14.1619853 116.742132,15.7769118 L108.948529,15.7769118 C109.364559,13.5734559 110.687647,12.0070588 112.843015,12.0070588 Z M113.575147,22.6740441 C111.089779,22.6740441 109.432059,21.0648529 108.951397,18.7886029 L120.268897,18.7886029 C120.268897,18.7886029 120.359118,18.2199265 120.359118,17.5219853 C120.359118,12.5955882 117.873088,8.99779412 112.843015,8.99779412 C108.212647,8.99779412 105.228309,12.8305147 105.228309,17.3649265 C105.228309,21.8995588 107.833235,25.7322794 113.371544,25.7322794 C117.650956,25.7322794 120.385368,23.0490441 120.385368,23.0490441 L118.562206,20.6088971 C118.562206,20.6088971 116.440147,22.6740441 113.575147,22.6740441 Z M123.075441,25.3747059 L126.838235,25.3747059 L126.838235,3.77647059 L123.075441,3.77647059 L123.075441,25.3747059 Z M101.455147,3.44073529 C103.033897,3.44073529 104.846029,3.85786765 104.846029,3.85786765 L104.846029,7.05463235 C103.543676,6.56889706 99.9194118,5.55463235 99.9194118,8.63691176 L99.9194118,9.32580882 L104.846029,9.32580882 L104.846029,12.4252941 L99.9194118,12.4252941 L99.9194118,25.3747059 L96.1566176,25.3747059 L96.1566176,8.28286765 C96.1566176,3.59007353 99.8761765,3.44073529 101.455147,3.44073529 Z M90.7784559,3.44073529 C92.3574265,3.44073529 94.1695588,3.85786765 94.1695588,3.85786765 L94.1695588,7.05463235 C92.8669853,6.56889706 89.2429412,5.55463235 89.2429412,8.63691176 L89.2429412,9.32580882 L94.1695588,9.32580882 L94.1695588,12.4252941 L89.2429412,12.4252941 L89.2429412,25.3747059 L85.4801471,25.3747059 L85.4801471,12.4252941 L82.6991912,12.4252941 L82.6991912,9.32580882 L85.4801471,9.32580882 L85.4801471,8.28286765 C85.4801471,3.59007353 89.1997059,3.44073529 90.7784559,3.44073529 Z M76.7435294,9.32580882 L80.5065441,9.32580882 L80.5065441,25.3747059 L76.7435294,25.3747059 L76.7435294,23.1769853 C75.8360294,24.2486029 74.1485294,25.7025 71.6695588,25.7025 C67.7441912,25.7025 65.7531618,22.9614706 65.7531618,19.8097059 C65.7531618,16.6577206 65.7531618,9.32580882 65.7531618,9.32580882 L69.5159559,9.32580882 C69.5159559,9.32580882 69.5159559,17.0084559 69.5159559,18.7367647 C69.5159559,20.4652941 70.3590441,22.2675 73.0402941,22.2675 C74.7961765,22.2675 76.0032353,21.1544118 76.7435294,19.7693382 L76.7435294,9.32580882 Z M52.6661029,21.8799265 L47.9258824,21.8799265 L47.9258824,8.00845588 L52.6661029,8.00845588 C56.4963971,8.00845588 59.2879412,11.1138971 59.2879412,14.9444118 C59.2879412,18.7749265 56.4963971,21.8799265 52.6661029,21.8799265 Z M52.6661029,4.51367647 L44.1035294,4.51367647 L44.1035294,25.3749265 L52.6661029,25.3749265 C58.4265441,25.3749265 63.0963971,20.7048529 63.0963971,14.9444118 C63.0963971,9.18352941 58.4265441,4.51367647 52.6661029,4.51367647 L52.6661029,4.51367647 Z" class="LockUp_logo__2C0BU"></path>
-                <path d="M-3.69482223e-13,7.47220588 L-3.69482223e-13,22.4163971 C2.06338235,22.4163971 3.73610294,19.0709559 3.73610294,14.9441912 C3.73610294,10.8176471 2.06338235,7.47220588 -3.69482223e-13,7.47220588 Z M7.10757353,4.51367647 L7.10757353,25.3749265 C11.2343382,25.3749265 14.5797794,20.7048529 14.5797794,14.9441912 C14.5797794,9.18352941 11.2343382,4.51367647 7.10757353,4.51367647 Z M32.8954412,14.9441912 C32.8954412,23.1977206 26.2047794,29.8883824 17.95125,29.8883824 L17.95125,-3.55271368e-15 C26.2047794,-3.55271368e-15 32.8954412,6.69088235 32.8954412,14.9441912 L32.8954412,14.9441912 Z" fill="#EE4159" class="LockUp_mark__1i8eI"></path>
-            </svg>
+            <a href="<?=root?>">
+             <img src="<?=assets?>img/logo.png" class="" />
             </a>
         </div>
         <ul class="HSpace_h-space__1C_k1 HSpace_h-space--lg__1G4Eg Header_header__nav--links__1aTYi">
             <li>
             <div>
-                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="#">Product</a>
+                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="<?=root?>product">Product</a>
                 <div></div>
             </div>
             </li>
             <li>
             <div>
-                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="#">Developers</a>
+                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="<?=root?>developers">Developers</a>
                 <div></div>
             </div>
             </li>
-            <li><a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="/pricing">Pricing</a></li>
+            <li><a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="<?=root?>pricing">Pricing</a></li>
             <li>
             <div>
-                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="#">Company</a>
+                <a class="Anchor_anchor__u3Dra Anchor_anchor--white__3vXHo" href="<?=root?>contact-us">Contact</a>
                 <div></div>
             </div>
             </li>
